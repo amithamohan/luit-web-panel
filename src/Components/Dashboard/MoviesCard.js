2113186c3ab8e4e7cbd10f3df62401aec229b5e1
amithamohan <damitha.mohan@gmail.com>
2021-04-26 20:33:40 +0530

    video player and view of section for movies by language added

--- src/Components/Dashboard/MoviesCard.js
+++ src/Components/Dashboard/MoviesCard.js
@@ -8,7 +8,6 @@ import Popup from '../Utlities/PopUp';
 import Modal from 'antd/lib/modal/Modal';
 
 
-<<<<<<< HEAD
 function MoviesCard(props)
 {
 	const [isModalVisible, setIsModalVisible] = useState(false);
@@ -31,127 +30,12 @@ function MoviesCard(props)
 	};
 
 	const options =
-=======
-
-// class MoviesCard extends Component
-// {
-// 	constructor(props)
-// 	 
-// 		{
-// 			cards.push
-// 			(
-// 				<div className="owl-items" key={i}>
-// 					<a className="slide-one" href="/detailed_page">
-// 						<div className="slide-image"><img src="https://release.luit.co.in/uploads/movie_thumbnail/thumb_1609755203JONBAI%201" alt="" /></div>
-// 						<div className="slide-content">
-// 						<h2>{this.props.moviesList[i]["title"]}<img src="images/plus.png" className="add-wishlist" alt="" /></h2>
-// 						<p>Radhe is a singing prodigy determined to follow in the classical footsteps of his grandfather.</p>
-// 						<span className="tag">2 h 20 min</span>
-// 						<span className="tag">2020</span>
-// 						<span className="tag"><b>16+</b></span>
-// 						</div>
-// 					</a>
-// 				</div>
-// 			);
-// 		}
-// 		return(
-// 			<div>
-// 				<div className="slide-wrapper">
-// 					<div className="container">
-// 						<div className="row">
-// 							<div className="col-sm-6 text-left mb-4 mt-4">
-// 							<h2>Latest Movies</h2>
-// 							</div>
-// 						</div>
-// 						<div className="row">
-// 							<div className="col-sm-12">
-// 							<div className="slide-slider owl-carousel owl-theme">
-// 								<div className="owl-items">
-// 								<a className="slide-one" href="/detailed_page">
-									
-// 								{
-// 									cards
-// 								}
-// 								</a>
-// 								</div>
-// 								</div>
-								
-// 							</div>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</div>
-// 		);
-// 	}
-// }
-
-
-const data= [
-    {
-        title: "Made in haven",
-        image: "https://release.luit.co.in/uploads/movie_thumbnail/thumb_1609755203JONBAI%201",
-        description:"Radhe is a singing prodigy determined to follow in the     classical footsteps of his grandfather.",
-        totalTime:'2 h 20 min',
-        year:'2020',
-        Quality: "HD",
-        Views: "16+"
-    },
-    {
-        title: "Gravity",
-        image: "https://release.luit.co.in/uploads/movie_thumbnail/thumb_1609755203JONBAI%201",
-        description:"Radhe is a singing prodigy determined to follow in the     classical footsteps of his grandfather.",
-        totalTime:'2 h 20 min',
-        year:'2021',
-        Quality: "HD+",
-        Views: "16+"
-    },
-    {
-        title: "Inspector",
-        image: "https://release.luit.co.in/uploads/movie_thumbnail/thumb_1609755203JONBAI%201",
-        description:"Radhe is a singing prodigy determined to follow in the     classical footsteps of his grandfather.",
-        totalTime:'2 h 20 min',
-        year:'2020',
-        Quality: "HD",
-        Views: "16+"
-    },
-    {
-        title: "Sky Staar ",
-        image: "https://release.luit.co.in/uploads/movie_thumbnail/thumb_1609755203JONBAI%201",
-        description:"Radhe is a singing prodigy determined to follow in the     classical footsteps of his grandfather.",
-        totalTime:'2 h 20 min',
-        year:'2020',
-        Quality: "HD",
-        Views: "16+"
-    },
-
-]
-
-
-
-
-class MoviesCard extends Component
-{
-<<<<<<< HEAD
-    render()
-    {
-		
-    
-    return(
-        <div>
-        <div className="slide-wrapper search-wrap-slide mt-4">
-            <div className="container">
-				<div className="row">
-					<div className="col-sm-6 text-left mb-4 mt-1">
-						<h2>Latest Movies</h2>
-=======
-	options =
->>>>>>> 0acc2aea684ca5fa69c6fed6dad821f1a1f89006
 	{
 		items: 5,
 		margin: 5,
 		itemsDesktop: [1000, 5],
 		nav: true,
-		loop: false,
+		loop: true,
 		autoplay: true
 	};
 
@@ -188,7 +72,7 @@ class MoviesCard extends Component
 			{
 				cards.push(
 					<div key={i}>
-						<Link className="slide-one" to={{pathname: "/movies_detailed_page", params:{item: props.moviesList[i]}}} style={{height: "400px"}}>
+						<Link className="slide-one" to={{pathname: "", params:{item: props.moviesList[i]}}} style={{height: "400px"}}>
 							<div className="slide-image">
 								<img src={movie["thumbnail"]} alt={movie["movie_title"]} onError={(e)=>{e.target.onerror = null; e.target.src="https://release.luit.co.in/uploads/music_thumbnail/default.jpg"}} style={{maxWidth:"100%", height: "auto"}} />
 							</div>
@@ -230,10 +114,8 @@ class MoviesCard extends Component
 								cards
 							}
 						</OwlCarousel>
->>>>>>> a6397a38308ffc69a66761b7bc8494ba40d2987d
 					</div>
 				</div>
-<<<<<<< HEAD
 
 				<Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
 					<p>Some contents...</p>
@@ -242,39 +124,6 @@ class MoviesCard extends Component
 				</Modal>
 			</div>
 		);
-=======
-                <div className="row">
-				<div className="col-sm-12">
-				<div className="slide-slider owl-carousel owl-theme">
-					{
-					data.map((item) =>{
-                        return(
-                            <div className="col-md-12 col-lg-12 mb-3">
-                            <a className="slide-one" href="">
-                                <div className="slide-image"><img src={item.image} alt="image"/></div>
-                                <div className="slide-content">
-                                    <h2>{item["title"]} <img src="images/plus.png" alt="icon"/></h2>
-                                    <p>{item.description}</p>
-                                    <span className="tag">{item.totalTime}</span>
-                                    <span className="tag">{item.year}</span>
-                                    <span className="tag"><b>{item.Quality}</b></span>
-                                    <span className="tag"><b>{item.Views}</b></span>
-                                </div>
-                            </a>
-                            </div>
-                            );
-						}
-						
-					)}
-			</div>
-			</div>
-			</div>
-		</div>
-	</div>
-	</div>
-    )
-    }
->>>>>>> 0acc2aea684ca5fa69c6fed6dad821f1a1f89006
 }
 
 export default MoviesCard;
