b2979ae16f9d2decd5ddb0680362df0bc1530ba6
Suvradeep Paria <49704600+Suvradeepparia3@users.noreply.github.com>
2021-05-18 16:54:31 +0530

    remove user data from localhost

--- src/Components/Dashboard/NavBar.js
+++ src/Components/Dashboard/NavBar.js
@@ -120,9 +120,8 @@ class NavigationBar extends Component
 
     async logOut()
     {
-        let user = localStorage.getItem("user");
-
-        let data = JSON.parse(user);
+        
+        localStorage.clear();
 
         this.setState({isLoggedIn : false});
     }
@@ -179,7 +178,7 @@ class NavigationBar extends Component
                         <Dropdown overlay = 
                             {<Menu>
                                 <Menu.Item>
-                                    {this.state.isLoggedIn ? <a rel="noopener noreferrer" href="/demo/luitWeb/build/sign_in">
+                                    {this.state.isLoggedIn ? <a rel="noopener noreferrer" href="/demo/luitWeb/build/sign_in" onClick={e => {this.logOut()}}>
                                         Logout
                                     </a> : <a rel="noopener noreferrer" href="/demo/luitWeb/build/sign_in">
                                         Login
