6c62641a7ab5f44276d4c52d6dc9d6dd23b81a65
amithamohan <damitha.mohan@gmail.com>
2021-05-19 12:00:57 +0530

    done with payment

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import VideoPlayer from './Components/VideoPlayer/VideoPlayer';
 import StarRating from './Components/Dashboard/StarRating';
 import Subscribe from './Components/Subscribe';
 import SuccessPopup from './Components/Utlities/SuccessPopup';
+import ViewContents from './Components/Dashboard/ViewContents';
 
 function App() {
   return (
@@ -42,6 +43,7 @@ function App() {
         <Route path="/rating" component={StarRating} />
         <Route path="/subscribe" component={Subscribe} />
         <Route path="/successPopup" component={SuccessPopup} />
+        <Route path="/view-content" component={ViewContents} />
       </Switch>
     </Router>
 
